ec0a9b0db8bff7186245aee5229a999f14ef5410
arifulhoque7 <hoquea57@gmail.com>
2023-09-19 21:39:14 +0600

    Final commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# How To Install
+
+## Step 1: Install Dependencies
+
+Before installing the this project, we need to make sure that our system has all the required dependencies installed. We will need to install the following dependencies:
+
+- node(version - 16)
+
+## Step 2: Go To project directory and composer install
+
+- first go to the project directory
+
+```bash
+cd project-name
+```
+
+
+## Step 3: Now run this command
+
+```bash
+npm i
+```
+
+## Step 4: To see the final output run this command
+
+```bash
+npm run dev
+```
--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,21 +1,112 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Modal from "react-modal";
 
 const Problem2 = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [contacts, setContacts] = useState([]);
+  const [isUS, setIsUS] = useState(false);
+  const [onlyEven, setOnlyEven] = useState(false);
 
-    return (
+  useEffect(() => {
+    fetchContacts();
+  }, [isUS, onlyEven]);
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
-                <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
-                </div>
-                
-            </div>
+  const fetchContacts = async () => {
+    let url = isUS
+      ? "https://contact.mediusware.com/api/country-contacts/United%20States/?page=1&page_size=600"
+      : "https://contact.mediusware.com/api/contacts/?page=1&page_size=600";
+    const response = await axios.get(url);
+    let data = response.data.results;
+    if (onlyEven) {
+      data = data.filter((contact) => contact.id % 2 === 0);
+    }
+    setContacts(data);
+  };
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
+        <div className="d-flex justify-content-center gap-3">
+          <button
+            className="btn btn-lg btn-outline-primary"
+            type="button"
+            style={{ backgroundColor: "#46139f" }}
+            onClick={() => {
+              setIsUS(false);
+              setModalIsOpen(true);
+            }}
+          >
+            All Contacts
+          </button>
+          <button
+            className="btn btn-lg btn-outline-warning"
+            type="button"
+            style={{ backgroundColor: "#ff7f50" }}
+            onClick={() => {
+              setIsUS(true);
+              setModalIsOpen(true);
+            }}
+          >
+            US Contacts
+          </button>
         </div>
-    );
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={() => setModalIsOpen(false)}
+        >
+          <button
+            style={{ backgroundColor: "#46139f" }}
+            onClick={() => {
+              setIsUS(false);
+              fetchContacts();
+            }}
+          >
+            All Contacts
+          </button>
+          <button
+            style={{ backgroundColor: "#ff7f50" }}
+            onClick={() => {
+              setIsUS(true);
+              fetchContacts();
+            }}
+          >
+            US Contacts
+          </button>
+          <button
+            style={{ backgroundColor: "white", borderColor: "#46139f" }}
+            onClick={() => setModalIsOpen(false)}
+          >
+            Close
+          </button>
+          <input
+            type="checkbox"
+            id="evenOnly"
+            name="evenOnly"
+            onChange={(e) => {
+              setOnlyEven(e.target.checked);
+            }}
+          />
+          <label for="evenOnly"> Only even</label>
+          <input
+            type="text"
+            id="search"
+            name="search"
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+          />
+          {contacts.map((contact) => (
+            <div key={contact.id}>
+              <p>{contact.phone}</p>
+              <p>{contact.country.name}</p>
+            </div>
+          ))}
+        </Modal>
+      </div>
+    </div>
+  );
 };
 
 export default Problem2;
